c308f85dca17ce92b47ae00607443fc42a3c31d9
Mael Donnart <maeldonnart@hotmail.fr>
2024-12-06 10:19:29 +0100

    add Fantasia image in about section add metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,45 @@ export const metadata = {
   title: 'FTM Versus! - Premier Tournament Hosting on Fantom Blockchain',
   description:
     'FTM Versus! is the premier tournament hosting platform on the Fantom Blockchain, supporting the growing web3 gaming ecosystem.',
+  keywords: 'FTM Versus, FTM, Versus, Tournament, Hosting, Blockchain',
+  authors: [{ name: 'Maël Donnart', url: 'https://www.maeldonnart.fr/' }],
+  openGraph: {
+    title: 'FTM Versus! - Premier Tournament Hosting on Fantom Blockchain',
+    description:
+      'FTM Versus! is the premier tournament hosting platform on the Fantom Blockchain, supporting the growing web3 gaming ecosystem.',
+    url: 'https://www.ftmversus.com/',
+    siteName: 'FTM Versus! - Premier Tournament Hosting on Fantom Blockchain',
+    images: [
+      {
+        url: 'https://www.ftmversus.com/logo.webp',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'FTM Versus! - Premier Tournament Hosting on Fantom Blockchain',
+    description:
+      'FTM Versus! is the premier tournament hosting platform on the Fantom Blockchain, supporting the growing web3 gaming ecosystem.',
+    images: ['https://www.ftmversus.com/logo.webp'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  category: 'games',
 }
 
 export default function RootLayout({
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function About() {
   const cards = [
     {
@@ -18,16 +20,26 @@ export default function About() {
   ]
 
   return (
-    <section className="py-20 bg-gray-100">
+    <section className="py-6 bg-gray-100">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-center mb-8">
           About FTM Versus!
         </h2>
+        <div className="my-4 flex justify-center items-center">
+          <Image
+            src="/fantasia-moon.webp"
+            alt="FTM Versus Logo"
+            width={512}
+            height={512}
+            loading="lazy"
+          />
+        </div>
         <p className="text-xl text-center mb-12">
           FTM Versus! is home to all things competitive on the Fantom
           blockchain. Our goal is to help support the growing web3 gaming
           ecosystem on Fantom by bringing awareness through competition!
         </p>
+
         <div className="grid md:grid-cols-3 gap-8">
           {cards.map((item, index) => (
             <div key={index} className="bg-white rounded-lg shadow-md p-6">
@@ -35,6 +47,15 @@ export default function About() {
               <p>{item.description}</p>
             </div>
           ))}
+        </div>
+        <div className="my-4 flex justify-center items-center">
+          <Image
+            src="/fantasia-money.webp"
+            alt="FTM Versus Logo"
+            width={512}
+            height={512}
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
